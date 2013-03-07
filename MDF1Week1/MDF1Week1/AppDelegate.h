f434092985d078671df5e0d7b9c515a9a6c5155c
JustinRowe15 <JustinRowe15@fullsail.edu>
2013-03-07 16:19:06 -0500

    Initial commit for Mobile Development Frameworks Week 1 Project

---
+++ MDF1Week1/MDF1Week1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MDF1Week1
+//
+//  Created by Justin Rowe on 3/7/13.
+//  Copyright (c) 2013 Justin Rowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MDF1Week1/MDF1Week1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDF1Week1
+//
+//  Created by Justin Rowe on 3/7/13.
+//  Copyright (c) 2013 Justin Rowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
